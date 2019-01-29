2a95b374dc3ed0065a65a6c8b7a723919a28d422
yjh458 <yjh458@naver.com>
2019-01-29 12:13:35 +0900

    0129 12:13 메인레이아웃 / 편의시설dto추가/룸정보수정

---
+++ src/main/java/com/bit/geha/dto/FacilityDto.java
@@ -0,0 +1,12 @@
+package com.bit.geha.dto;
+
+import lombok.Data;
+
+@Data
+public class FacilityDto {
+	private int guestHouseCode;
+	private int facilityCode;
+	private String facilityName;
+
+	
+}
