b55387992e7ec9eb6849a80bfa09a279aab0263c
yjh458 <yjh458@naver.com>
2019-01-29 10:28:20 +0900

    0129 10:27 commit/merge 테스트

--- src/main/java/com/bit/geha/controller/RoomController.java
+++ src/main/java/com/bit/geha/controller/RoomController.java
@@ -1,5 +1,15 @@
 package com.bit.geha.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class RoomController {
 
+	
+	@RequestMapping("/room")
+	public String roomInfo() {
+		
+		return "roomInfo";
+	}
 }
--- src/main/java/com/bit/geha/dto/RoomDto.java
+++ src/main/java/com/bit/geha/dto/RoomDto.java
@@ -1,14 +1,12 @@
 package com.bit.geha.dto;
 
-import org.apache.ibatis.type.Alias;
-
 import lombok.Data;
 
-@Alias("room")
 @Data
 public class RoomDto {
 
 	private int roomCode ;
+	private int guestHouseCode;
 	private String roomName;
 	private int capacity;
 	private int charge;
