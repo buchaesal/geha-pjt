75998ddda93da13a3e536747265094979c1fac2f
Jonny-Cho <andante2183@gmail.com>
2019-01-30 16:27:47 +0900

    16:27 폰트추가 & search.html 수정

--- src/main/java/com/bit/geha/controller/SearchController.java
+++ src/main/java/com/bit/geha/controller/SearchController.java
@@ -1,11 +1,27 @@
 package com.bit.geha.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bit.geha.criteria.SearchCriteria;
+import com.bit.geha.dao.SearchDao;
 
 @Controller
 public class SearchController {
 
+	@Autowired
+	private SearchDao dao;
+	
+	/*@RequestMapping("search")
+	public String search(SearchCriteria sc, Model model) {
+		
+		model.addAttribute("list", dao.listGeha(sc));
+		
+		return "search";
+	}*/
+	
 	@RequestMapping("search")
 	public String search() {
 		return "search";
