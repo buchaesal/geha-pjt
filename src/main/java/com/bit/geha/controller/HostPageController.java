9bc73786624f34ddd3acf8cac2feceb4e60df9d0
KimSeulKee <tmfrl6545@hanmail.net>
2019-01-30 10:56:37 +0900

    0130 10:55

--- src/main/java/com/bit/geha/controller/HostPageController.java
+++ src/main/java/com/bit/geha/controller/HostPageController.java
@@ -3,12 +3,17 @@ package com.bit.geha.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.java.Log;
+
 @Controller
 @RequestMapping("/hostPage")
+@Log
 public class HostPageController {
 	
 	@RequestMapping("/myGuestHouseList")
-	public void myGuestHouseList(int memberCode) {
+	public void myGuestHouseList() {
+		log.info("myGuestHouseList");
+		
 		
 	}
 }
