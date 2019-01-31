14cb42059da6c56c76226076f7104f97ecbce032
Jonny-Cho <andante2183@gmail.com>
2019-01-31 17:40:00 +0900

    0131 17:38 RestApi / Json / Handlebars 이용한 데이터 출력 완성

--- src/main/java/com/bit/geha/controller/SearchController.java
+++ src/main/java/com/bit/geha/controller/SearchController.java
@@ -1,12 +1,17 @@
 package com.bit.geha.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bit.geha.criteria.SearchCriteria;
 import com.bit.geha.dao.SearchDao;
+import com.bit.geha.dto.SearchDto;
 
 @Controller
 public class SearchController {
@@ -14,17 +19,19 @@ public class SearchController {
 	@Autowired
 	private SearchDao dao;
 	
-	/*@RequestMapping("search")
-	public String search(SearchCriteria sc, Model model) {
-		
-		model.addAttribute("list", dao.listGeha(sc));
-		
-		return "search";
-	}*/
-	
 	@RequestMapping("search")
-	public String search() {
+	public String search(SearchCriteria sc, Model model) {
+		model.addAttribute("list", dao.listGeha());
+		/*model.addAttribute("list", dao.searchGeha(sc));*/
 		return "search";
 	}
+	
+	@GetMapping("search/gehainfo")
+	@ResponseBody
+	public List<SearchDto> searchapi(SearchCriteria sc){
+		return dao.listGeha();
+	}
+	
+	
 	
 }
--- src/main/java/com/bit/geha/dto/SearchDto.java
+++ src/main/java/com/bit/geha/dto/SearchDto.java
@@ -5,12 +5,16 @@ import lombok.Data;
 @Data
 public class SearchDto {
 
-	private String originalName;
 	private int guestHouseCode;
 	private String guestHouseName;
 	private double avgRating;
 	private String recommendRating;
-	private int reviewNo;
-	private int charge;
+	private int minprice;
+	private int businesstrip;
+	private int gourmet;
+	private int trip;
+	private int shopping;
+	private int reviewCnt;
+	private String originalName;
 	
 }
