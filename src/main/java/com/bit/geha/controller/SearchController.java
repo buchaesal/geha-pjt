8ef5907dab6e71b338e43e996048ab528d87360e
Jonny-Cho <andante2183@gmail.com>
2019-02-08 12:46:54 +0900

    ajax 검색 완성

--- src/main/java/com/bit/geha/controller/SearchController.java
+++ src/main/java/com/bit/geha/controller/SearchController.java
@@ -3,10 +3,14 @@ package com.bit.geha.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -17,39 +21,45 @@ import com.bit.geha.dto.SearchDto;
 @Controller
 public class SearchController {
 
+	Logger logger = LoggerFactory.getLogger(SearchController.class);
+	
 	@Autowired
 	private SearchDao dao;
 	
 	@RequestMapping("/search")
 	public String search(Model model) {
-		model.addAttribute(dao.listGeha());
+		logger.info("search()");
 		return "search";
 	}
 	
 	@GetMapping("/allgehainfo")
 	@ResponseBody
 	public List<SearchDto> searchapi(SearchCriteria sc){
+		logger.info("/allgehainfo()");
 		return dao.listGeha();
 	}
 	
-	@GetMapping("/searchgehainfo")
+	@PostMapping(path = "/searchgehainfo", consumes = "application/json", produces = "application/json")
 	@ResponseBody
-	public List<SearchDto> searchGehaInfo(SearchCriteria sc){
-		System.out.println("searchGehaInfo()");
+	public List<SearchDto> searchGehaInfo(@RequestBody SearchCriteria sc){
+		logger.info("/searchGehaInfo()");
 		
+		// null exception for gender and facilities
 		if(sc.getGender() == null) {
 			List<String> gender = new ArrayList<>();
 			sc.setGender(gender);
 		}
-		System.out.println(sc.getGender());
-		System.out.println(sc.getGender().size());
 		
 		if(sc.getFacilities() == null) {
 			List<Integer> facilities = new ArrayList<>();
 			sc.setFacilities(facilities);
 		}
+		
+		System.out.println(sc.getKeyword());
+		System.out.println(sc.getGender());
 		System.out.println(sc.getFacilities());
-		System.out.println(sc.getFacilities().size());
+		System.out.println(sc.getMinprice());
+		System.out.println(sc.getMaxprice());
 		
 		return dao.searchGeha(sc);
 	}
