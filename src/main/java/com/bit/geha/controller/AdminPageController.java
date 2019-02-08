47b4cbb1f421d00d46fa8334e985bd7cfeb2a3cc
eks41 <eks41@DESKTOP-9OEN3UE>
2019-02-08 23:02:14 +0900

    0208 11:02 리뷰관리 완성

--- src/main/java/com/bit/geha/controller/AdminPageController.java
+++ src/main/java/com/bit/geha/controller/AdminPageController.java
@@ -1,12 +1,23 @@
 package com.bit.geha.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.bit.geha.criteria.AdminPageCriteria;
 import com.bit.geha.dao.AdminPageDao;
+import com.bit.geha.service.MemberService;
+import com.bit.geha.util.PageMaker;
 
 @Controller
 @RequestMapping("/adminPage")
@@ -15,9 +26,47 @@ public class AdminPageController {
 	@Autowired
 	AdminPageDao adminPageDao;
 	
-	@RequestMapping("/memberList")
-	public void memberList(Model model) {
-		model.addAttribute("list",adminPageDao.getMemberList());
+	@Autowired
+	MemberService memberService;
+	
+	@RequestMapping("/adminPage")
+	public void adminPage() {
+	}
+	
+	@GetMapping("/adminPage")
+	public void select(@ModelAttribute("cri") AdminPageCriteria cri,String auth,
+			Model model) {
+		PageMaker pageMaker = new PageMaker();
+		pageMaker.setCri(cri);
+		
+		if(!auth.equals("all")) {
+			model.addAttribute("list",adminPageDao.getMemberList(cri,auth));
+			model.addAttribute("auth",auth);
+			pageMaker.setTotalCount(adminPageDao.getTotal(cri,auth));
+		}else {
+		model.addAttribute("list",adminPageDao.getMemberList(cri,""));
+		model.addAttribute("auth","");
+		pageMaker.setTotalCount(adminPageDao.getTotal(cri,""));
+		}
+		
+		
+		model.addAttribute("pageMaker", pageMaker);
+		
+	}
+	
+	@GetMapping("/memberList")
+	public String memberList(@ModelAttribute("cri") AdminPageCriteria cri,Model model,Authentication auth,HttpSession session) {
+		memberService.getSession(auth, session);
+		
+		model.addAttribute("list",adminPageDao.getMemberList(cri,""));
+		
+		PageMaker pageMaker = new PageMaker();
+		pageMaker.setCri(cri);
+		pageMaker.setTotalCount(adminPageDao.getTotal(cri,""));
+		
+		model.addAttribute("pageMaker", pageMaker);
+		model.addAttribute("auth","");
+		return "/adminPage/adminPage";
 	}
 	
 	@RequestMapping("/changeAdmin")
@@ -30,5 +79,13 @@ public class AdminPageController {
 	public String changeUser(@RequestParam("id") String id) {
 		adminPageDao.changeUser(id);
 		return "redirect:/adminPage/memberList";
+	}
+	
+	@PostMapping("/authSelect")
+	@ResponseBody
+	public String authSelect(String param) {
+		System.out.println(param);
+		
+		return "/adminPage/adminPage?auth="+param;
 	}
 }
--- src/main/java/com/bit/geha/controller/MyPageController.java
+++ src/main/java/com/bit/geha/controller/MyPageController.java
@@ -1,5 +1,10 @@
 package com.bit.geha.controller;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,13 +12,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.bit.geha.dao.MemberDao;
 import com.bit.geha.dao.MyPageDao;
-import com.bit.geha.dto.MemberDto;
 import com.bit.geha.dto.ReviewDto;
 import com.bit.geha.security.SecurityMember;
 import com.bit.geha.service.MemberService;
@@ -72,18 +78,21 @@ public class MyPageController {
 		return "redirect:/myPage/bookingList?memberCode="+memberCode;
 	}
 	
+	//리뷰관리
 	@RequestMapping("/myReview")
 	public void myReview(Model model,HttpSession session) {
 		int memberCode=((Integer) session.getAttribute("memberCode")).intValue();
 		model.addAttribute("reviewList",myPageDao.getReviewList(memberCode));
 	}
 	
+	//내정보관리
 	@RequestMapping("/myInfo")
 	public void myInfo(Model model,Authentication auth) {
 		SecurityMember sc = (SecurityMember) auth.getPrincipal();
 		model.addAttribute("memberDto",memberDao.findById(sc.getUsername()));
 	}
 	
+	//내정보수정
 	@RequestMapping("/updateInfo")
 	public String updateInfo(@RequestParam String id,@RequestParam String memberName,@RequestParam String password,
 			@RequestParam(required=false) String businessLicense, @RequestParam String gender) {
@@ -102,14 +111,41 @@ public class MyPageController {
 			
 	}
 	
+	//리뷰 수정
 	@RequestMapping("/modifyReview")
 	public String modifyReview(ReviewDto reviewDto) {
 		myPageDao.modifyReview(reviewDto);
 		return "redirect:/myPage/myReview";
 	}
 	
+	//리뷰 삭제
 	@RequestMapping("/deleteReview")
-	public String deleteReview() {
+	public String deleteReview(@RequestParam int reviewNo,Model model) {
+		/*myPageDao.deleteReview(reviewNo);*/
+		model.addAttribute("deleteInfo","선택한 후기가 삭제되었습니다.");
 		return "redirect:/myPage/myReview";
+	}
+	
+	//찜한숙소
+	@RequestMapping("/myLike")
+	public void myLike(Model model,HttpSession session) {
+		int memberCode=((Integer) session.getAttribute("memberCode")).intValue();
+		model.addAttribute("likeList",myPageDao.myLike(memberCode));
+		
+	}
+	
+	
+	//찜 삭제
+	@RequestMapping("/deleteLike")
+	public String deleteLike(HttpServletRequest request,HttpSession session,
+			RedirectAttributes redirectAttributes) {
+		int memberCode=((Integer) session.getAttribute("memberCode")).intValue();
+		String[] arr = request.getParameterValues("chk");
+		List<String> list=Arrays.asList(arr);
+		List<Integer> deleteLikeList = list.stream().map(Integer::parseInt)
+				.collect(Collectors.toList());
+		myPageDao.deleteLike(memberCode,deleteLikeList);
+		redirectAttributes.addFlashAttribute("deleteOk","선택한 찜이 삭제되었습니다.");
+		return "redirect:/myPage/myLike";
 	}
 }
