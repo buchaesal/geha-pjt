c003d9a991a8980db566f52bf9bff5a7e85568a1
KimSeulKee <tmfrl6545@hanmail.net>
2019-02-13 16:02:20 +0900

    0213 16:01 게스트하우스 수정하기 기능구현, 게스트하우스 파일 업로드

--- src/main/java/com/bit/geha/controller/HostPageController.java
+++ src/main/java/com/bit/geha/controller/HostPageController.java
@@ -1,5 +1,6 @@
 package com.bit.geha.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -116,14 +117,147 @@ public class HostPageController {
 		log.info("modifyGuestHouse()");
 		
 		model.addAttribute("guestHouseDto", hostPageDao.getGuestHouse(guestHouseCode));
-		model.addAttribute("rooms", hostPageDao.getRooms(guestHouseCode));
+		List<RoomDto> roomList = hostPageDao.getRooms(guestHouseCode);
 		model.addAttribute("facilities", hostPageDao.getFacilities(guestHouseCode));
-		List<FileDto> guestHouseImgs = hostPageDao.getGuestHouseImgs(guestHouseCode);
-		for(FileDto img : guestHouseImgs) {
-			System.out.println(img.getSavedName());
-		}
-		model.addAttribute("guestHouseImgs", guestHouseImgs);
+		model.addAttribute("guestHouseImgs", hostPageDao.getImgs(guestHouseCode, 0));
 		
+		for(int i=0; i<roomList.size(); i++) {
+			roomList.get(i).setRoomImgs(hostPageDao.getImgs(guestHouseCode, roomList.get(i).getRoomCode()));
+		}
+		model.addAttribute("rooms", roomList);
+	}
+	
+	@RequestMapping("modifyGuestHouseComplete")
+	public String modifyGuestHouseComplete(GuestHouseDto guestHouseDto, RoomDtos roomDtos, @RequestParam List<Integer> facilityCode) throws Exception {
+		log.info("modifyGuestHouseComplete()");
+
+		System.out.println("GuestHouseCode: " + guestHouseDto.getGuestHouseCode());
+		
+		/*
+		 * 게스트하우스 무조건 update
+		 * File savedName이 하나라도 있으면 안바뀜 (mainImage도 변경 여부) / 없으면 바뀜 기존-delete 수정-insert
+		 * facility 무조건 update
+		 * room 하나하나 비교
+		 * 수정(roomCode != 0) - File : savedName이 하나라도 있으면 안바뀜 / 없으면 바뀜 기존-delete 수정-insert mainImage도 변경
+		 * 					 - RoomDto : 무조건 update
+		 * 삭제(!roomCode.exists())  - File : 무조건 delete
+		 * 							- RoomDto : 무조건  delete
+		 * 추가(roomCode == 0) - File : 무조건 insert
+		 * 					  - RoomDto : 무조건 insert 
+		*/
+		
+		//게스트하우스 update
+		hostPageDao.modifyGuestHouse(guestHouseDto);
+		
+		List<FileDto> existingGuestHouseImgs = hostPageDao.getImgs(guestHouseDto.getGuestHouseCode(), 0);
+		
+		if(guestHouseDto.getFiles().get(0).getSize() == 0) { //기존이미지였을 경우
+			System.out.println("게스트하우스 기존이미지");
+			
+			if(!(existingGuestHouseImgs.get(guestHouseDto.getMainImage()).isMainImage())) { //기존 메인인덱스랑 다를 경우
+				for(int i=0; i<existingGuestHouseImgs.size(); i++) {
+					System.out.println("메인인덱스랑 다름");
+					if(i==guestHouseDto.getMainImage()) {
+						existingGuestHouseImgs.get(i).setMainImage(true);
+						hostPageDao.modifyMainImage(existingGuestHouseImgs.get(i));
+					} else {
+						existingGuestHouseImgs.get(i).setMainImage(false);
+						hostPageDao.modifyMainImage(existingGuestHouseImgs.get(i));
+					}
+				}
+			}
+		} else { //새로운 이미지일 경우
+			System.out.println("게스트하우스 새로운이미지");
+			
+			//파일 delete
+			UploadFileUtils.deleteFiles(existingGuestHouseImgs);
+			hostPageDao.deleteImgs(guestHouseDto.getGuestHouseCode(), 0);
+			
+			//파일 insert
+			List<FileDto> guestHouseFiles = UploadFileUtils.uploadFiles(guestHouseDto);
+			hostPageDao.addFiles(guestHouseFiles);
+		}
+
+		
+		//편의시설 delete and insert
+		hostPageDao.deleteFacilities(guestHouseDto.getGuestHouseCode());
+		Map<String, Object> facilityMap = new HashMap<>();
+		facilityMap.put("guestHouseCode", guestHouseDto.getGuestHouseCode());
+		facilityMap.put("facilityCode", facilityCode);
+		hostPageDao.addFacilities(facilityMap);
+		
+		
+		//수정화면에서 업로드 된 방
+		List<RoomDto> rooms = roomDtos.inputRoomDtoValue(guestHouseDto.getGuestHouseCode());
+		
+		List<RoomDto> newRooms = new ArrayList<>(); //추가 방 List
+		List<Integer> existingRoomCodes = hostPageDao.getRoomCodes(guestHouseDto.getGuestHouseCode());
+		
+		for(RoomDto room : rooms) {
+			if(existingRoomCodes.contains(room.getRoomCode())) { //수정되었을 경우
+				System.out.println("기존 룸 : " + room.getRoomCode());
+				
+				existingRoomCodes.remove((Integer)room.getRoomCode()); //존재하는 것들은 지움
+				
+				List<FileDto> existingRoomImgs = hostPageDao.getImgs(room.getGuestHouseCode(), room.getRoomCode());
+
+				if(room.getRoomFiles()==null || room.getRoomFiles().get(0).getSize() == 0) { //기존이미지였을 경우
+					System.out.println("방 기존이미지");
+					
+					if(room.getMainImage()!=-1) { //기존 메인인덱스랑 다를 경우
+						for(int i=0; i<existingRoomImgs.size(); i++) {
+							System.out.println("메인인덱스랑 다름");
+							if(i==room.getMainImage()) {
+								existingRoomImgs.get(i).setMainImage(true);
+								hostPageDao.modifyMainImage(existingRoomImgs.get(i));
+							} else {
+								existingRoomImgs.get(i).setMainImage(false);
+								hostPageDao.modifyMainImage(existingRoomImgs.get(i));
+							}
+						}
+					}
+				} else { //새로운 이미지일 경우
+					System.out.println("방 새로운이미지");
+					
+					//파일 delete
+					UploadFileUtils.deleteFiles(existingRoomImgs);
+					hostPageDao.deleteImgs(room.getGuestHouseCode(), room.getRoomCode());
+					
+					//파일 insert
+					List<FileDto> roomFiles = UploadFileUtils.uploadFiles(room);
+					hostPageDao.addFiles(roomFiles);
+				}
+				
+				//방 update
+				hostPageDao.modifyRoom(room);
+			} else {
+				//추가 되었을 경우
+				newRooms.add(room);
+			}
+		}
+		
+		//방 insert
+		if(newRooms.size()>0) {
+			hostPageDao.addRooms(newRooms);
+			
+			for(RoomDto roomDto : newRooms) {
+				List<FileDto> roomFiles = UploadFileUtils.uploadFiles(roomDto);
+				hostPageDao.addFiles(roomFiles);
+			}
+		}
+		
+		//삭제되었을 경우 - File, Room delete
+		System.out.println("delete roomCode: " + existingRoomCodes);
+		if(existingRoomCodes.size()>0) {
+			hostPageDao.deleteRooms(existingRoomCodes);
+			for (int i = 0; i < existingRoomCodes.size(); i++) {
+				System.out.println("delete Img");
+				hostPageDao.deleteImgs(guestHouseDto.getGuestHouseCode(), existingRoomCodes.get(i));
+				UploadFileUtils.deleteRoomFiles(guestHouseDto.getGuestHouseCode(), existingRoomCodes.get(i));
+			}
+		}
+		
+		return "redirect:myGuestHouseList";
 	}
 	
 	@RequestMapping(value="guestBookingList")
