8fa7e48cd871312824b99a43aa02eaacdca041e1
eks4116 <eks4116@gmail.com>
2019-02-18 15:01:26 +0900

    0218 15:01 home 관리자 페이지 url 수정

--- src/main/java/com/bit/geha/controller/SearchController.java
+++ src/main/java/com/bit/geha/controller/SearchController.java
@@ -5,9 +5,12 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,9 +35,9 @@ public class SearchController {
 	MemberService memberService;
 	
 	@RequestMapping("/search")
-	public String search(SearchCriteria sc, Model model) {
+	public String search(SearchCriteria sc, Model model,Authentication auth,HttpSession session) {
 		logger.info("search()");
-		
+		memberService.getSession(auth, session);
 		model.addAttribute("sc", sc);
 		
 		return "/search";
